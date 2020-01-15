f3d3d35cac39904e42819a7605a23f3e0854a6b3
Cixi Geng <cixi.geng1@unisoc.com>
2020-01-15 09:35:32 +0800

    update scripts

--- run.sh
+++ run.sh
@@ -1,35 +1,58 @@
 #!/bin/bash
 
 TOPDIR=$(cd `dirname $0`;pwd)
-echo ${TOPDIR}
+# check return error
+check_err()
+{
+        if [ $? -ne 0 ]; then
+                echo -e "\033[31m FAIL: $* \033[0m" >&2
+                return_val=1
+                return
+        else
+                echo -e "\033[33m PASS: $* \033[0m" >&2
+                return_val=0
+        fi
+}
+build_busybox()
+{
+	echo ${ROOTFS}
+	cd busybox-1.31.1
+	cp ../unisoc_defconfig configs/
+	make unisoc_defconfig
+	make
+	check_err "make busybox"
+
+	if [ ${return_val} -eq 0 ]; then
+		make install
+		check_err "install busybox"
+	fi
+	mv ./_install ../${ROOTFS}
+}
+clean_busybox()
+{
+	echo -e "\033[33m clean up busybox \033[0m"
+	cd ${TOPDIR}
+	rm -rf busybox-1.31.1
+	git checkout busybox-1.31.1
+}
 
 
-ROOTFS=rootfs-$1
-if [ -d ${ROOTFS} ];then
-    rm -rf ${ROOTFS}
+if [ $# -eq 1 ]; then
+	export ROOTFS=rootfs-$1
+	if [ -d "${ROOTFS}" ];then
+	    rm -rf ${ROOTFS}
+	fi
+	if [ $1=arm ];then
+	    ARCH=arm
+	    CROSS_COMPILE=arm-linux-gnueabi-
+	elif [ $1=arm64 ];then
+	    ARCH=arm64
+	    CROSS_COMPILE=aarch64-linux-gnu-
+	fi
+	build_busybox
+	clean_busybox
 fi
 
-if [ $1=arm ];then 
-    ARCH=arm
-    CROSS_COMPILE=arm-linux-gnueabi-
-elif [ $1=arm64 ];then
-    ARCH=arm64
-    CROSS_COMPILE=aarch64-linux-gnu-
-fi
-
-if [ -d busybox ];then
-    rm -rf busybox 
-fi
-
-
-cd busybox-1.30.1
-
-cp ../unisoc_defconfig configs/
-make unisoc_defconfig
-make 
-make install
-mv ./_install ../${ROOTFS}
 
 cd ${TOPDIR}
-
-./config.sh $1
+bash ${TOPDIR}/config.sh $1
